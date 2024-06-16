8a169a441c5bdd2bfd08071d1803f92ff1910875
0nam1 <damonte@naver.com>
2024-06-16 15:22:35 +0900

    수정 커밋 푸시2

--- dags/dags_conn_test1.py
+++ dags/dags_conn_test1.py
@@ -7,13 +7,41 @@ with DAG(
 	schedule="None", #크론탭 문법으로 스케쥴 지정
 ) as dag:
 	t1 = EmptyOperator(
-		task_id="empty_t1", #테스크 명 지정
-		empty_command="echo empty1", #실제 실행되는 수행 과업
+		task_id="empty_t1" #테스크 명 지정
 	)
 
 	t2 = EmptyOperator(
-		task_id="empty_t2", #테스크 명 지정
-		empty_command="echo empty2", #실제 실행되는 수행 과업
+		task_id="empty_t2" #테스크 명 지정
+	)
+
+	t3 = EmptyOperator(
+		task_id="empty_t3" #테스크 명 지정
+	)
+
+	t4 = EmptyOperator(
+		task_id="empty_t4" #테스크 명 지정
+	)
+
+	t5 = EmptyOperator(
+		task_id="empty_t5" #테스크 명 지정
+	)
+
+	t6 = EmptyOperator(
+		task_id="empty_t6" #테스크 명 지정
+	)
+
+	t7 = EmptyOperator(
+		task_id="empty_t7" #테스크 명 지정
+	)
+
+	t8 = EmptyOperator(
+		task_id="empty_t8" #테스크 명 지정
 	)
 
 	t1 >> t2 # 테스간 실행 순서
+	t1 >> t3
+	t3 >> t4
+	t5 >> t4
+	t4 >> t6
+	t7 >> t6
+	t6 >> t8
