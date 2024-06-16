786641c48db0bac393e84b7f511be2d276948f4b
0nam1 <damonte@naver.com>
2024-06-16 15:19:45 +0900

    수정 커밋 푸시

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -1,19 +1,23 @@
+
 from airflow import DAG
+import datetime
 import pendulum
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
 
 with DAG(
 	dag_id="dags_bash_operator", #Dag이름 지정
-	schedule="None", #크론탭 문법으로 스케쥴 지정
+	schedule="0 0 * * *", #크론탭 문법으로 스케쥴 지정
+	start_date=pendulum.datetime(2024, 6, 14, tz="Asia/Seoul"), #Dag 시작 날짜
+	catchup=True # 누락된 스케쥴을 따라잡을것인지 여부
 ) as dag:
-	t1 = EmptyOperator(
-		task_id="empty_t1", #테스크 명 지정
-		empty_command="echo empty1", #실제 실행되는 수행 과업
+	bash_t1 = BashOperator(
+		task_id="bash_t1", #테스크 명 지정
+		bash_command="echo whoami", #실제 실행되는 수행 과업
 	)
 
-	t2 = EmptyOperator(
-		task_id="empty_t2", #테스크 명 지정
-		empty_command="echo empty2", #실제 실행되는 수행 과업
+	bash_t2 = BashOperator(
+		task_id="bash_t2",
+		bash_command="echo $HOSTNAME",
 	)
 
-	t1 >> t2 # 테스간 실행 순서
+	bash_t1 >> bash_t2 # 테스간 실행 순서
